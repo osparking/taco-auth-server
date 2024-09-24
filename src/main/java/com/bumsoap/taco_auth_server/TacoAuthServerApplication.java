fdc3a58fa839c788ae784a051616dd8eae514312
osparking <jbpark03@gmail.com>
2024-09-24 12:03:07 +0900

    authorization server for taco cloud

---
+++ src/main/java/com/bumsoap/taco_auth_server/TacoAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.taco_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumsoap/taco_auth_server/TacoAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumsoap.taco_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
