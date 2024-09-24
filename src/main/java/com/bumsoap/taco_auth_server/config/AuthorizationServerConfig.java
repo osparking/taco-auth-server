e7a3cc9099930cec16f2ec1cc769fd05a2afa47e
osparking <jbpark03@gmail.com>
2024-09-24 19:33:36 +0900

    add class to import some common server config'

---
+++ src/main/java/com/bumsoap/taco_auth_server/config/AuthorizationServerConfig.java
@@ -0,0 +1,24 @@
+package com.bumsoap.taco_auth_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
+
+        return http
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
