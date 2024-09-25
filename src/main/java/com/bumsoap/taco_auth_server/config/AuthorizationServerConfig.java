2f019a5f957c5750000064b28180e159dc67f80c
osparking <jbpark03@gmail.com>
2024-09-25 09:38:02 +0900

    make client repository with taco-admin-client

--- src/main/java/com/bumsoap/taco_auth_server/config/AuthorizationServerConfig.java
+++ src/main/java/com/bumsoap/taco_auth_server/config/AuthorizationServerConfig.java
@@ -6,8 +6,18 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
 
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
@@ -20,5 +30,26 @@ public class AuthorizationServerConfig {
         return http
                 .formLogin(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(
+            PasswordEncoder passwordEncoder) {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("taco-admin-client")
+                .clientSecret(passwordEncoder.encode("1234"))
+                .clientAuthenticationMethod(
+                        ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri(
+                        "http://127.0.0.1:9090/login/oauth2/code/taco-admin-client")
+                .scope("writeIngredients")
+                .scope("deleteIngredients")
+                .scope(OidcScopes.OPENID)
+                .clientSettings(ClientSettings.builder()
+                                .requireAuthorizationConsent(true).build())
+                .build();
+        return new InMemoryRegisteredClientRepository(registeredClient);
     }
 }
